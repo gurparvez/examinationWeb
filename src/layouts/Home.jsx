c838856ef5a9b69b8546011bedf5fed5e52a4c94
gurparvez <gurparvez04@gmail.com>
2024-02-05 15:40:39 +0530

    increase home about responsiveness

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -4,7 +4,7 @@ import { aboutUniversity } from '../constants'
 
 function Home() {
   return (
-    <div className='flex lg:flex-row flex-col py-4 px-9 bg-home justify-evenly'>
+    <div className='flex lg:flex-row flex-col py-4 xs:px-6 ss:px-9 bg-home justify-evenly'>
       <img src={home1} alt="about" className='lg:w-[50%] p-[15px] rounded-[50px] object-cover aspect-[33/26]' />
       <div className='lg:w-[50%] p-[15px]'>
         <h3 className='text-[18px] text-primary font-bold font-jost'>{aboutUniversity[0].heading}</h3>
@@ -12,7 +12,7 @@ function Home() {
         <p className='text-p font-jost text-[18px] leading-7 font-semibold' >{aboutUniversity[0].info1}</p>
         <p className='text-content font-jost text-[16px] leading-7 font-medium'>{aboutUniversity[0].info2}</p>
         <div className='ss:m-[15px] xs:m-[10px] my-[5px]'>
-          <ul className='flex xs:flex-row flex-col flex-wrap *:flex *:flex-row *:xs:max-w-[50%] *:max-w-full'>
+          <ul className='flex ss:flex-row flex-col flex-wrap *:flex *:flex-row *:ss:max-w-[50%] *:py-2 *:max-w-full'>
             {aboutUniversity[0].content.map((card) => (
               <li>
                 <div className='bg-primary max-h-[55px] text-white font-bold py-[15px] px-[18px] rounded-[50%]'>{card.id}</div>
