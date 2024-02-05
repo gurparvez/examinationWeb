80fb9e5f6f4691cde94635a1603e3c312576b0e8
gurparvez <gurparvez04@gmail.com>
2024-02-05 15:47:17 +0530

    exmination button text color white

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ data = "button" }) => {
   return (
-    <button className='bg-primary w-auto border-none rounded py-1 px-2 hover:cursor-pointer hover:bg-secondary hover:text-white transition-all ease-in-out' >
+    <button className='bg-primary w-auto border-none rounded py-1 px-2 text-white hover:cursor-pointer hover:bg-secondary transition-all ease-in-out' >
       {data}
     </button>
   )
