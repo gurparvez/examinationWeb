70d094d4b29416b57f810f082b98d0398c1e3a9b
gurparvez <gurparvez04@gmail.com>
2024-02-05 14:21:56 +0530

    navbar undeline animation

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,14 @@ function App() {
       <div className='sticky z-50 top-0 bg-white shadow-lg'>
         <nav className='flex flex-row justify-between px-8'>
           <ul className='flex flex-row p-3.5'>
-            <li className='flex items-center list-none'><NavLink to="https://auts.ac.in/" target='_blank' ><img src={logo} alt="AUTS" className="h-[40px] xs:h-[50px] sm:h-full" /></NavLink></li>
+            <li className='flex items-center list-none'><NavLink to="https://auts.ac.in/" target='_blank' ><img src={logo} alt="AUTS" className="h-[40px] xs:h-[50px] sm:h-full hover:scale-105 transition-all ease-in-out duration-300" /></NavLink></li>
           </ul>
           <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
             {navLinks.map((nav, index) => (
               <li key={nav.id} className={`font-jost font-normal cursor-pointer ${index === (navLinks.length-1) ? 'mr-0' : 'mr-10'} text-[18px]`} >
-                <NavLink to={`${nav.id}`}>
+                <NavLink to={`${nav.id}`} className="relative group hover:text-primary">
                   {nav.title}
+                  <span className="absolute inset-x-0 bottom-0 h-0.5 bg-primary origin-left transform scale-x-0 group-hover:scale-x-100 transition-transform duration-250"></span>
                 </NavLink>
               </li>
             ))}
--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,6 @@ import announcement_img from './announcements1.jpeg'
 import logo from './logo.png'
 import close from './close.svg'
 import menu from './menu.svg'
+import home1 from './home1.png'
 
-export { login_img, announcement_img, logo, close, menu }
+export { login_img, announcement_img, logo, close, menu, home1 }
