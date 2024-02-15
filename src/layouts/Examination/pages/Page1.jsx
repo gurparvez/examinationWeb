8438f9bfcf50f9dd3320ef1985589bd4429dbc04
Snimarpal Singh <snimarryan@gmail.com>
2024-02-15 19:04:35 +0530

    start page 2

--- src/layouts/Examination/pages/Page1.jsx
+++ src/layouts/Examination/pages/Page1.jsx
@@ -9,27 +9,27 @@ const Page1 = () => {
 
     const [ispopup, setIspopup] = useState(false)
 
-    const [auid, setAuid] = useState()
-    const [fullName, setFullName] = useState()
-    const [phoneNumber, setPhoneNumber] = useState()
-    const [email, setEmail] = useState()
-    const [fatherName, setFatherName] = useState()
-    const [motherName, setMotherName] = useState()
-    const [address, setAddress] = useState()
+    const [auid, setAuid] = useState('')
+    const [fullName, setFullName] = useState('')
+    const [phoneNumber, setPhoneNumber] = useState('')
+    const [email, setEmail] = useState('')
+    const [fatherName, setFatherName] = useState('')
+    const [motherName, setMotherName] = useState('')
+    const [address, setAddress] = useState('')
     const [profileImage, setProfileImage] = useState(profile)
 
     const user = useSelector(state => state.auth.userData);
     const userData = user.user
     useEffect(() => {
         if (userData) {
-            setAuid(userData.auid)
-            setFullName(userData.fullName)
-            setPhoneNumber(userData.phoneNumber)
-            setEmail(userData.email)
-            setFatherName(userData.fatherName)
-            setMotherName(userData.motherName)
-            setAddress(userData.address)
-            setProfileImage(userData.avatar)
+            setAuid(userData.auid || '')
+            setFullName(userData.fullName || '')
+            setPhoneNumber(userData.phoneNumber || '')
+            setEmail(userData.email || '')
+            setFatherName(userData.fatherName || '')
+            setMotherName(userData.motherName || '')
+            setAddress(userData.address || '')
+            setProfileImage(userData.avatar || '')
         }
     }, [])
 
--- src/layouts/Examination/pages/Page2.jsx
+++ src/layouts/Examination/pages/Page2.jsx
@@ -1,13 +1,25 @@
-import { Input } from 'postcss'
 import React from 'react'
+import { Input } from '../../../components'
 
 const Page2 = () => {
   return (
     <div className='px-6 xs:px-10 sm:px-14 py-8'>
-        <h1 className='text-xl font-bold'>Personal Info :</h1>
+        <h1 className='text-xl font-bold'>Courses Info :</h1>
         <div className='bg-gray-200 rounded-lg'>
-            <div>
-                <Input />
+            <div className='flex flex-col sm:flex-row w-full *:my-3 *:sm:my-1 p-5'>
+                <Input label="Subject" />
+                <div className='hidden sm:block h-9 w-[1px] bg-black mx-2 box-border'></div>
+                <Input label="Subject" />
+            </div>
+            <div className='flex flex-col sm:flex-row w-full *:my-3 *:sm:my-1 p-5'>
+                <Input label="Subject" />
+                <div className='hidden sm:block h-9 w-[1px] bg-black mx-2 box-border'></div>
+                <Input label="Subject" />
+            </div>
+            <div className='flex flex-col sm:flex-row w-full *:my-3 *:sm:my-1 p-5'>
+                <Input label="Subject" />
+                <div className='hidden sm:block h-9 w-[1px] bg-black mx-2 box-border'></div>
+                <Input label="Subject" />
             </div>
         </div>
     </div>
