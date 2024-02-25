10bef993bfe7155ccd6224312ae5e186712db5c2
Snimarpal Singh <snimarryan@gmail.com>
2024-02-25 10:21:03 +0530

    user not found 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Outlet, NavLink, Link } from "react-router-dom"
-import { logo, close, menu, profile, notFound } from './assets'
+import { logo, close, menu, profile, notFound2 } from './assets'
 import { Button } from "./components"
 import { navLinks } from "./constants"
 import { useState, useEffect } from "react"
@@ -66,14 +66,15 @@ function App() {
       </div>
 
       {isLoggedin ? <Outlet /> : 
-      <div className="flex flex-col sm:flex-row mx-10 my-6 justify-center">
+      <div className="flex flex-col sm:flex-row mx-5 my-6 justify-center">
         <div className="w-[100%] sm:w-[70%]">
-          <img src={notFound} alt="error: 404"  />
+          <img src={notFound2} alt="error: 404"  />
         </div>
-        <div className="flex flex-col justify-center items-center sm:w-[30%] w-[100%] bg-lightGray p-8">
-          <div className="*:m-4">
-            <h1 className="text-3xl font-bold text-red-600">User Not Found</h1>
-            <Link to="/"><Button data="Please Login" className="text-xl" /></Link>
+        <div className="flex flex-col justify-center items-center sm:w-[30%] w-[100%] p-8">
+          <div className="*:my-4 sm:*:m-4">
+            <h1 className="text-4xl sm:text-6xl font-bold">Whoops! That Page is Gone</h1>
+            <p>The link you clicked may be broken or the page may have been removed. You can try to login again or get back to the home page.</p>
+            <Link to="/"><Button data="Login" className="text-xl" /></Link>
           </div>
         </div>
       </div>}
