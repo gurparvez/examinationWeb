01fb8506353c845266761f9aa89ae5da9b7711db
Nareshkumar124 <nkk124421@gmail.com>
2024-03-05 16:54:19 +0530

    change in api

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "https://examform.onrender.com"
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     "/api": "https://examform.onrender.com"
+  //   }
+  // }
 })
