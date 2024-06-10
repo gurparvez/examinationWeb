49ec02400b9359b6de0c30faeff37ea30e5c176f
Snimarpal Singh <snimarryan@gmail.com>
2024-06-10 20:25:59 +0530

    more unit tests on User API class

--- tests/API/User.test.js
+++ tests/API/User.test.js
@@ -23,12 +23,21 @@ describe('User Login', () => {
         expect(response.message).toBe('auid and password is requried.');
     });
 
-    it('Should handle failure if auid or password not provided', async () => {
+    it('Should handle failure if user not found', async () => {
         const response = await user.login({
-            auid: '222706030',
-            // password: 'Naresh@123',
+            auid: '222706031',
+            password: 'Naresh@123',
         });
 
-        expect(response.message).toBe('auid and password is requried.');
+        expect(response.message).toBe('User does not exist');
+    });
+
+    it('Should handle failure if password is not correct', async () => {
+        const response = await user.login({
+            auid: '222706030',
+            password: 'Naresh@12',
+        });
+
+        expect(response.message).toBe('Password is not valid');
     });
 });
